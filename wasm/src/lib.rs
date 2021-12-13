1245f59cb99e78cbb9383a7b25dca22abe39114a
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2021-12-13 19:25:32 +0900

    wasm

---
+++ wasm/src/lib.rs
@@ -0,0 +1,22 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+pub use wasm_bindgen_rayon::init_thread_pool;
+
+// ...
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, wasm!");
+}
